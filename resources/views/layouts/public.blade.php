3e6d4360aec2748f2093ede4322d2d12e45ded43
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2026-07-19 10:59:48 +0800

    chore: add google site verification meta tag

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="StYM1sKESNQwJFWnPoB0bp9RWBW-JVOp8mmjEW76A-c" />
     <title>@yield('title', 'vnews.id - Berita Visual Indonesia')</title>
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link rel="canonical" href="{{ request()->url() }}">
