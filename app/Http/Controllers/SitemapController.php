545f98c0a449626cb2a5034561b10e6b8c7b371c
Imam Ahmad Askalani <ahmad.imam22@gmail.com>
2026-07-19 11:28:28 +0800

    fix: generate sitemap XML entirely in controller, remove blade dependency

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class SitemapController extends Controller
 {
@@ -13,9 +12,47 @@ class SitemapController extends Controller
         $posts = Post::where('status', 'Published')->orderBy('created_at', 'desc')->get();
         $categories = Category::all();
 
-        return response()->view('sitemap_xml', [
-            'posts' => $posts,
-            'categories' => $categories
-        ])->header('Content-Type', 'text/xml');
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+
+        // Homepage
+        $xml .= "  <url>\n";
+        $xml .= '    <loc>' . url('/') . "</loc>\n";
+        $xml .= '    <lastmod>' . now()->tz('UTC')->toAtomString() . "</lastmod>\n";
+        $xml .= "    <changefreq>daily</changefreq>\n";
+        $xml .= "    <priority>1.0</priority>\n";
+        $xml .= "  </url>\n";
+
+        // About page
+        $xml .= "  <url>\n";
+        $xml .= '    <loc>' . url('/about') . "</loc>\n";
+        $xml .= '    <lastmod>' . now()->tz('UTC')->toAtomString() . "</lastmod>\n";
+        $xml .= "    <changefreq>monthly</changefreq>\n";
+        $xml .= "    <priority>0.5</priority>\n";
+        $xml .= "  </url>\n";
+
+        // Categories
+        foreach ($categories as $category) {
+            $xml .= "  <url>\n";
+            $xml .= '    <loc>' . route('category.show', $category->slug) . "</loc>\n";
+            $xml .= '    <lastmod>' . ($category->updated_at ?? now())->tz('UTC')->toAtomString() . "</lastmod>\n";
+            $xml .= "    <changefreq>weekly</changefreq>\n";
+            $xml .= "    <priority>0.8</priority>\n";
+            $xml .= "  </url>\n";
+        }
+
+        // Posts
+        foreach ($posts as $post) {
+            $xml .= "  <url>\n";
+            $xml .= '    <loc>' . route('posts.show', $post->slug) . "</loc>\n";
+            $xml .= '    <lastmod>' . ($post->updated_at ?? now())->tz('UTC')->toAtomString() . "</lastmod>\n";
+            $xml .= "    <changefreq>daily</changefreq>\n";
+            $xml .= "    <priority>0.9</priority>\n";
+            $xml .= "  </url>\n";
+        }
+
+        $xml .= '</urlset>';
+
+        return response($xml, 200)->header('Content-Type', 'text/xml; charset=UTF-8');
     }
 }
